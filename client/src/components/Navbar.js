6df8ab585105ee2e387dbaa688f5e0f6a48dfaef
popemray00 <popemray1986@gmail.com>
2019-04-02 20:19:35 -0700

    added target blank for facebook and insta

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -14,8 +14,8 @@ const NavBar = () => {
       <NavLink className="Link" to="/wod">Wod</NavLink>
       <NavLink className="Link" to="/momstrong">Mom Strong</NavLink>
       <div className="icons">
-      <a href="https://www.facebook.com/CrossFit-Dismantle-1524864784486908/"><FontAwesomeIcon icon={['fab', 'facebook']} size="5x" className="Facebook" /></a>
-      <a href="https://www.instagram.com/crossfitdismantle/"><FontAwesomeIcon icon={['fab', 'instagram']} size="5x" className="Instagram" /></a>
+      <a href="https://www.facebook.com/CrossFit-Dismantle-1524864784486908/" target="_blank"><FontAwesomeIcon icon={['fab', 'facebook']} size="5x" className="Facebook" /></a>
+      <a href="https://www.instagram.com/crossfitdismantle/" target="_blank"><FontAwesomeIcon icon={['fab', 'instagram']} size="5x" className="Instagram" /></a>
       </div>
     </div>
     <div>
