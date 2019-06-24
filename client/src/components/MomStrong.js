8db177eb3f1a83562aef366c1fd1157ba5ac4570
popemray00 <popemray1986@gmail.com>
2019-06-24 13:46:42 -0700

    fixed momstrong error

--- client/src/components/MomStrong.js
+++ client/src/components/MomStrong.js
@@ -12,55 +12,55 @@ import './MomStrong.css'
         </div>
 
         <div className="Bottom-Image">
-          <div class="gallery">
+          <div className="gallery">
             <a target="_blank" href="images/image8.jpg">
               <img src="images/image8.jpg" alt="image8" width= "600" height="400"/>
             </a>
           </div>
 
-          <div class="gallery">
+          <div className="gallery">
             <a target="_blank" href="images/image9.jpg">
               <img src="images/image9.jpg" alt="image9" width="600" height="400"/>
             </a>
           </div>
 
-          <div class="gallery">
+          <div className="gallery">
             <a target="_blank" href="images/image11.jpg">
               <img src="images/image11.jpg" alt="image11" width="600" height="400"/>
             </a>
           </div>
 
-          <div class="gallery">
+          <div className="gallery">
             <a target="_blank" href="images/image10.jpg">
               <img src="images/image10.jpg" alt="image10" width="600" height="400"/>
             </a>
           </div>
 
-          <div class="gallery">
+          <div className="gallery">
             <a target="_blank" href="images/image12.jpg">
               <img src="images/image12.jpg" alt="image12" width="600" height="400"/>
             </a>
           </div>
 
-          <div class="gallery">
+          <div className="gallery">
             <a target="_blank" href="images/image13.jpg">
               <img src="images/image13.jpg" alt="image13" width="600" height="400"/>
             </a>
           </div>
 
-          <div class="gallery">
+          <div className="gallery">
             <a target="_blank" href="images/image14.jpg">
               <img src="images/image14.jpg" alt="image14" width="600" height="400"/>
             </a>
           </div>
 
-          <div class="gallery">
+          <div className="gallery">
             <a target="_blank" href="images/image15.jpg">
               <img src="images/image15.jpg" alt="image15" width="600" height="400"/>
             </a>
           </div>
 
-          <div class="gallery">
+          <div className="gallery">
             <a target="_blank" href="images/image16.jpg">
               <img src="images/image16.jpg" alt="image16" width="600" height="400"/>
             </a>
