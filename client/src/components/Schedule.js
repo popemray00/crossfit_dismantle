f513a84a3d6980080b8fb9b37116450133498a07
popemray00 <popemray1986@gmail.com>
2019-06-24 13:43:46 -0700

    schedule changed again

--- client/src/components/Schedule.js
+++ client/src/components/Schedule.js
@@ -13,13 +13,16 @@ import './Schedule.css'
 
         <div className="Hours">
           <h1 className="Hours-Title">CrossFit Hours</h1>
-          <h1>Monday - Friday  5 AM, 6 AM, 5:30 PM & 6:30 PM</h1>
-          <h1>Saturday 10 AM</h1>
+          <h1>Monday - Friday</h1>
+          <h2>5 AM, 6 AM, 5:30 PM & 6:30 PM</h2>
+          <h1>Saturday</h1>
+          <h2>10 AM</h2>
         </div>
 
         <div className="Hours2">
           <h1 className="Hours-Title">Mom Strong</h1>
-          <h1>Monday - Friday  9:00am</h1>
+          <h1>Monday - Friday</h1>
+          <h2>9 AM</h2>
         </div>
       </div>
     );
